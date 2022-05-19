bdcb927ec12f7134859a47c3eb73c0a4ccfeac9f
José Matos <josemnmatos@gmail.com>
2022-05-19 01:35:03 +0100

    script

--- postgresql/BD2022_data.sql
+++ postgresql/BD2022_data.sql
@@ -5,19 +5,19 @@
 	#
 */
 
-
 CREATE TABLE produto (
 	id SERIAL,
 	descricao				 VARCHAR(512),
 	tipo				VARCHAR(512),				
-	preco				 FLOAT(8) NOT NULL CHECK(stock>=0),
+	preco				 FLOAT(8) NOT NULL CHECK(preco>=0),
 	stock				 BIGINT NOT NULL CHECK(stock>=0),
 	vendedor_utilizador_id INTEGER NOT NULL,
 	PRIMARY KEY(id)
 );
 
+/*
 CREATE TABLE televisao (
-	tamanho	 BIGINT,
+	tamanho	 VARCHAR(512),
 	produto_id BIGINT,
 	PRIMARY KEY(produto_id)
 );
@@ -33,6 +33,30 @@ CREATE TABLE smartphone (
 	produto_id BIGINT,
 	PRIMARY KEY(produto_id)
 );
+*/
+------------------------------------------------
+CREATE TABLE especificacao_atual(
+	nome VARCHAR(512),
+	valor VARCHAR(512),
+	produto_id BIGINT,
+	PRIMARY KEY(produto_id,nome)
+);
+
+CREATE TABLE especificacao_antiga(
+	nome VARCHAR(512),
+	valor VARCHAR(512),
+	produto_id BIGINT,
+	PRIMARY KEY(produto_id,nome)
+);
+
+CREATE TABLE historico_preco(
+	preco FLOAT(8) NOT NULL CHECK(preco>=0),
+	data_exp TIMESTAMP,
+	produto_id BIGINT,
+	PRIMARY KEY(produto_id,data_exp)
+);
+-------------------------------------------------
+
 
 CREATE TABLE utilizador (
 	id		 SERIAL,
@@ -48,10 +72,6 @@ CREATE TABLE administrador (
 	utilizador_id INTEGER,
 	PRIMARY KEY(utilizador_id)
 );
-
---first admin
-INSERT INTO utilizador (username,email,password) VALUES ('admin','admin@gmail.com','password');
-INSERT INTO administrador (utilizador_id) SELECT id from utilizador WHERE username='admin';
 
 
 CREATE TABLE vendedor (
@@ -104,6 +124,8 @@ CREATE TABLE notificacao (
 	id		 SERIAL,
 	mensagem	 VARCHAR(512) NOT NULL,
 	estado_leitura BOOL NOT NULL,
+	data_rececao TIMESTAMP,
+	utilizador_id BIGINT,
 	PRIMARY KEY(id)
 );
 
@@ -114,6 +136,7 @@ CREATE TABLE item_encomenda (
 	PRIMARY KEY(encomenda_id,produto_id)
 );
 
+/*
 CREATE TABLE versao_antiga (
 	versao	 INTEGER,
 	ativo_ate	 TIMESTAMP,
@@ -142,33 +165,75 @@ CREATE TABLE vendedor_notificacao_encomenda (
 	notificacao_encomenda_notificacao_id BIGINT UNIQUE NOT NULL,
 	PRIMARY KEY(vendedor_utilizador_id)
 );
+*/
+
 
 /*
 ADD CONSTRAINTS FOR FOREIGN KEYS
 */
 ALTER TABLE produto ADD CONSTRAINT produto_fk1 FOREIGN KEY (vendedor_utilizador_id) REFERENCES vendedor(utilizador_id);
-ALTER TABLE televisao ADD CONSTRAINT televisao_fk1 FOREIGN KEY (produto_id) REFERENCES produto(id);
-ALTER TABLE computador ADD CONSTRAINT computador_fk1 FOREIGN KEY (produto_id) REFERENCES produto(id);
-ALTER TABLE smartphone ADD CONSTRAINT smartphone_fk1 FOREIGN KEY (produto_id) REFERENCES produto(id);
+--ALTER TABLE televisao ADD CONSTRAINT televisao_fk1 FOREIGN KEY (produto_id) REFERENCES produto(id);
+--ALTER TABLE computador ADD CONSTRAINT computador_fk1 FOREIGN KEY (produto_id) REFERENCES produto(id);
+--ALTER TABLE smartphone ADD CONSTRAINT smartphone_fk1 FOREIGN KEY (produto_id) REFERENCES produto(id);
 ALTER TABLE administrador ADD CONSTRAINT administrador_fk1 FOREIGN KEY (utilizador_id) REFERENCES utilizador(id);
 ALTER TABLE vendedor ADD CONSTRAINT vendedor_fk1 FOREIGN KEY (utilizador_id) REFERENCES utilizador(id);
 ALTER TABLE comprador ADD CONSTRAINT comprador_fk1 FOREIGN KEY (utilizador_id) REFERENCES utilizador(id);
 ALTER TABLE rating ADD CONSTRAINT rating_fk1 FOREIGN KEY (comprador_utilizador_id) REFERENCES comprador(utilizador_id);
 ALTER TABLE rating ADD CONSTRAINT rating_fk2 FOREIGN KEY (produto_id) REFERENCES produto(id);
-ALTER TABLE resposta ADD CONSTRAINT resposta_fk1 FOREIGN KEY (notificacao_resposta_notificacao_id) REFERENCES notificacao_resposta(notificacao_id);
+--ALTER TABLE resposta ADD CONSTRAINT resposta_fk1 FOREIGN KEY (notificacao_resposta_notificacao_id) REFERENCES notificacao_resposta(notificacao_id);
 ALTER TABLE resposta ADD CONSTRAINT resposta_fk2 FOREIGN KEY (thread_id) REFERENCES thread(id);
-ALTER TABLE encomenda ADD CONSTRAINT encomenda_fk1 FOREIGN KEY (notificacao_encomenda_notificacao_id) REFERENCES notificacao_encomenda(notificacao_id);
+--ALTER TABLE encomenda ADD CONSTRAINT encomenda_fk1 FOREIGN KEY (notificacao_encomenda_notificacao_id) REFERENCES notificacao_encomenda(notificacao_id);
 ALTER TABLE encomenda ADD CONSTRAINT encomenda_fk2 FOREIGN KEY (comprador_utilizador_id) REFERENCES comprador(utilizador_id);
 ALTER TABLE item_encomenda ADD CONSTRAINT item_encomenda_fk1 FOREIGN KEY (encomenda_id) REFERENCES encomenda(id);
 ALTER TABLE item_encomenda ADD CONSTRAINT item_encomenda_fk2 FOREIGN KEY (produto_id) REFERENCES produto(id);
-ALTER TABLE versao_antiga ADD CONSTRAINT versao_antiga_fk1 FOREIGN KEY (produto_id) REFERENCES produto(id);
-ALTER TABLE notificacao_resposta ADD CONSTRAINT notificacao_resposta_fk1 FOREIGN KEY (notificacao_id) REFERENCES notificacao(id);
-ALTER TABLE notificacao_encomenda ADD CONSTRAINT notificacao_encomenda_fk1 FOREIGN KEY (notificacao_id) REFERENCES notificacao(id);
-ALTER TABLE comprador_notificacao_resposta ADD CONSTRAINT comprador_notificacao_resposta_fk1 FOREIGN KEY (comprador_utilizador_id) REFERENCES comprador(utilizador_id);
-ALTER TABLE comprador_notificacao_resposta ADD CONSTRAINT comprador_notificacao_resposta_fk2 FOREIGN KEY (notificacao_resposta_notificacao_id) REFERENCES notificacao_resposta(notificacao_id);
-ALTER TABLE vendedor_notificacao_encomenda ADD CONSTRAINT vendedor_notificacao_encomenda_fk1 FOREIGN KEY (vendedor_utilizador_id) REFERENCES vendedor(utilizador_id);
-ALTER TABLE vendedor_notificacao_encomenda ADD CONSTRAINT vendedor_notificacao_encomenda_fk2 FOREIGN KEY (notificacao_encomenda_notificacao_id) REFERENCES notificacao_encomenda(notificacao_id);
+--ALTER TABLE versao_antiga ADD CONSTRAINT versao_antiga_fk1 FOREIGN KEY (produto_id) REFERENCES produto(id);
+--ALTER TABLE notificacao_resposta ADD CONSTRAINT notificacao_resposta_fk1 FOREIGN KEY (notificacao_id) REFERENCES notificacao(id);
+--ALTER TABLE notificacao_encomenda ADD CONSTRAINT notificacao_encomenda_fk1 FOREIGN KEY (notificacao_id) REFERENCES notificacao(id);
+--LTER TABLE comprador_notificacao_resposta ADD CONSTRAINT comprador_notificacao_resposta_fk1 FOREIGN KEY (comprador_utilizador_id) REFERENCES comprador(utilizador_id);
+--ALTER TABLE comprador_notificacao_resposta ADD CONSTRAINT comprador_notificacao_resposta_fk2 FOREIGN KEY (notificacao_resposta_notificacao_id) REFERENCES notificacao_resposta(notificacao_id);
+--ALTER TABLE vendedor_notificacao_encomenda ADD CONSTRAINT vendedor_notificacao_encomenda_fk1 FOREIGN KEY (vendedor_utilizador_id) REFERENCES vendedor(utilizador_id);
+--ALTER TABLE vendedor_notificacao_encomenda ADD CONSTRAINT vendedor_notificacao_encomenda_fk2 FOREIGN KEY (notificacao_encomenda_notificacao_id) REFERENCES notificacao_encomenda(notificacao_id);
 
 /*
-INSERT BASE VALUES
+BASE VALUES
 */
+--first admin
+INSERT INTO utilizador (username,email,password) VALUES ('admin','admin@gmail.com','password');
+INSERT INTO administrador (utilizador_id) SELECT id from utilizador WHERE username='admin';
+
+
+/*
+FUNCTIONS
+*/
+CREATE FUNCTION save_old_specification() RETURNS TRIGGER
+LANGUAGE PLPGSQL
+AS $$
+BEGIN
+INSERT INTO especificacao_antiga(nome,valor,produto_id) VALUES(old.nome,old.valor,old.produto_id);
+END;
+$$;
+
+
+CREATE FUNCTION notf_order() RETURNS TRIGGER
+LANGUAGE PLPGSQL
+AS $$
+BEGIN
+INSERT INTO notificacao(id,mensagem,estado_leitura,data_rececao) VALUES(DEFAULT,FORMAT("Fez uma nova encomenda:\nID:%s",new.id),FALSE,CURRENT_TIMESTAMP);
+END;
+$$;
+
+
+/*
+TRIGGERS
+*/
+
+CREATE TRIGGER notf_order_trigger
+AFTER INSERT ON encomenda
+FOR EACH ROW
+EXECUTE PROCEDURE notf_order();
+
+
+CREATE TRIGGER save_old_specification_trigger
+AFTER UPDATE ON especificacao_atual
+FOR EACH ROW
+EXECUTE PROCEDURE save_old_specification();
